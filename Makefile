8b92db46e940123eead7acad224efe9c4562b8d4
Glen Akins <ktmglen@glenakins.com>
2022-02-07 16:26:49 -0700

    add easier ability to compile for boards without ethernet

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 compile: 
-	arduino-cli compile --fqbn esp32:esp32:esp32-gateway esp32-dante-testino
+	arduino-cli compile --fqbn esp32:esp32:esp32-gateway:Revision=RevF esp32-dante-testino
 
 upload:
-	arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:esp32-gateway esp32-dante-testino
+	arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:esp32-gateway:Revision=RevF esp32-dante-testino
 
--- esp32-dante-test.ino
+++ esp32-dante-test.ino
@@ -1,12 +1,24 @@
-#define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
-#define ETH_PHY_POWER 5
+#ifdef ARDUINO_ESP32_GATEWAY_F
+  #pragma message "Compiling for Olimex ESP32 Gateway Rev F"
+  #define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
+  #define ETH_PHY_POWER 5
+#else
+  #pragma message "Compiling for wireless ESP32 boards"
+#endif
 
+#ifdef ARDUINO_ESP32_GATEWAY_F
 #include <ETH.h>
+#else
+#include <WiFi.h>
+#endif
+
 #include <vector>
 
 #include "AsyncUDP.h"
 #include "DanteDevice.h"
 #include "DanteDeviceList.h"
+
+#include "secrets.h"
 
 static volatile bool eth_connected = false;
 
@@ -19,17 +31,38 @@ void setup (void)
 	delay (100);
 	Serial.printf ("Hello, world\n\r");
 
+#ifdef ARDUINO_ESP32_GATEWAY_F
 	// set up ethernet
 	WiFi.onEvent(WiFiEvent);
 	ETH.begin();
 	Serial.printf ("Returned from ETH.connected.\n\r");
 
-	// set up Dante mDNS listener
-	devices.begin ();
-
 	// wait for ethernet to connect
 	while (!eth_connected) {
+		delay(500);
+		Serial.print(".");
 	}
+	Serial.println("");
+	Serial.println("Ethernet connected!");
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP()); 
+#else
+	// set up wireless
+	WiFi.begin (ssid, password);
+
+	// wait for wireless to connect
+	while(WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("");
+	Serial.println("WiFi connected!");
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP()); 
+#endif
+
+	// set up Dante mDNS listener
+	devices.begin ();
 
 	// seed random number generator
 	randomSeed ((uint32_t)IPAddress (224, 0, 0, 251));
@@ -50,6 +83,7 @@ void loop (void)
 }
 
 
+#ifdef ARDUINO_ESP32_GATEWAY_F
 void WiFiEvent(WiFiEvent_t event)
 {
   Serial.printf("[WiFi-event] event: %d\n", event);
@@ -88,3 +122,4 @@ void WiFiEvent(WiFiEvent_t event)
       break;
   }
 }
+#endif
